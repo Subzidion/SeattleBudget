d746de25af2bde2ed1d09d9073e82b7c0b34c357
Carl 'Subzidion' Hiltbrunner <me@carl.hiltbrunner.email>
2020-06-07 01:00:21 -0700

    Basic application that pulls Budget details from Seattle's Open Data program and creates a Sankey diagram showing fund distribution to city services.

---
+++ src/BudgetChart.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './budgetChart.css';
+import BudgetSankeyChart from './BudgetSankeyChart';
+
+function getApprovedAmountByService(service) {
+  return Object.keys(service).reduce((serviceBudget, department) => {
+    return serviceBudget + Object.keys(service[department]).reduce((departmentBudget, program) => {
+      return departmentBudget + service[department][program].reduce((programBudget, expense) => {
+        return programBudget + (parseInt(expense["approved_amount"], 10) || 0);
+      }, 0)
+    }, 0)
+  }, 0)
+}
+
+function BudgetChart(props) {
+  let result = Object.keys(props["data"]).reduce((obj, service) => {
+      obj["nodes"].push({
+        "id": service
+      });
+      obj["links"].push({
+        "source": "General Fund",
+        "target": service,
+        "value": getApprovedAmountByService(props["data"][service])
+      });
+      return obj;
+  }, {"nodes": [{"id": "General Fund"}], "links": []});
+
+
+  return (
+    <div className="chart-container">
+      <BudgetSankeyChart data={ result }/>
+    </div>
+  );
+}
+
+export default BudgetChart;
