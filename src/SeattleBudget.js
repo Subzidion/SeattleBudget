3ec82cc423cb7794d3bb16e7a0180b13c0405e30
Carl 'Subzidion' Hiltbrunner <me@carl.hiltbrunner.email>
2020-06-08 02:01:35 -0700

    Add Ant Design UI components.

--- src/SeattleBudget.js
+++ src/SeattleBudget.js
@@ -1,24 +1,48 @@
 import React, { useState, useEffect } from 'react';
+import 'antd/dist/antd.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import BudgetChart from './BudgetChart';
 import { budgetData } from './BudgetDataProvider';
 
+const { Header, Content } = Layout;
+
 function SeattleBudget() {
   const [data, setData] = useState();
-
+  
   useEffect(() => {
     budgetData.then(data => {
       setData(data);
     })
   }, []);
+
   if(data) {
     return (
-      <BudgetChart data={ data["2019"] }/>
+      <Layout style={{ height: '100vh' }}>
+        <Header className="header">
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">Seattle Municipal Budget</Menu.Item>
+            <Menu.Item key="2">About</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Seattle Budget</Breadcrumb.Item>
+              <Breadcrumb.Item>Overview</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content className="site-layout-background" style={{ margin: 0 }}>
+              <div className="site-layout-content">
+                <BudgetChart data={ data["2019"] }/>
+              </div>
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
     );
   } else {
-    return (<div>
-      "Loading..."
-    </div>)
+    return (<div>"Loading..."</div>)
   }
 }
-
+  
+    
 export default SeattleBudget;
