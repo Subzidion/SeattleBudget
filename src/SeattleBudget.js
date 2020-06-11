b7d45e97524f248bab7bf3029b12f320b0656b24
Carl 'Subzidion' Hiltbrunner <me@carl.hiltbrunner.email>
2020-06-11 02:20:26 -0700

    Add FiscalYear variable for future configuration.

--- src/SeattleBudget.js
+++ src/SeattleBudget.js
@@ -10,17 +10,18 @@ function SeattleBudget() {
   const [data, setData] = useState();
   const [graphData, setGraphData] = useState();
   const [node, setNode] = useState();
+  const [fiscalYear] = useState("2019");
 
   useEffect(() => {
       budgetData.then(data => {
-        setData(data["2019"]);
-        setGraphData(data["2019"]);
+        setData(data[fiscalYear]);
+        setGraphData(data[fiscalYear]);
       });
   }, [])
   
   useEffect(() => {
     if(node) {
-      setGraphData(graphData["children"][node]);
+      setGraphData(previousGraphData => previousGraphData["children"][node]);
     }
   }, [node]);
 
@@ -35,30 +36,30 @@ function SeattleBudget() {
     content = <BudgetChart data={ graphData } onNodeClick={ setNode } />
   }
 
-    return (
-      <Layout style={{ height: '100vh' }}>
-        <Header className="header">
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">Seattle Municipal Budget</Menu.Item>
-            <Menu.Item key="2">About</Menu.Item>
-          </Menu>
-        </Header>
-        <Layout>
-          <Layout style={{ padding: '0 24px 24px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>Seattle Budget { "2019" }</Breadcrumb.Item>
-              <Breadcrumb.Item>General Fund</Breadcrumb.Item>
-              <Breadcrumb.Item>{ node }</Breadcrumb.Item>
-            </Breadcrumb>
-            <Content className="site-layout-background" style={{ margin: 0 }}>
-              <div className="site-layout-content">
-                { content }
-              </div>
-            </Content>
-          </Layout>
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Header className="header">
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">Seattle Municipal Budget</Menu.Item>
+          <Menu.Item key="2">About</Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Seattle Budget { fiscalYear }</Breadcrumb.Item>
+            <Breadcrumb.Item>General Fund</Breadcrumb.Item>
+            <Breadcrumb.Item>{ node }</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content className="site-layout-background" style={{ margin: 0 }}>
+            <div className="site-layout-content">
+              { content }
+            </div>
+          </Content>
         </Layout>
       </Layout>
-    );
+    </Layout>
+  );
 }
   
     
